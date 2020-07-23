d277734e99e70426fa7999cdc94eada0b4a1debc
Karim <55461899+Moustamid@users.noreply.github.com>
2020-07-23 02:42:27 +0100

    Add files via upload

---
+++ ajax/getProgress.php
@@ -0,0 +1,19 @@
+<?php
+require_once("../includes/config.php");
+
+if(isset($_POST["videoId"]) && isset($_POST["username"]) ) {
+    $query = $connect->prepare(" SELECT progress FROM videoProgress 
+                                 WHERE username=:username AND videoId=:videoId ");
+
+    $query->bindValue(":username", $_POST["username"]);
+    $query->bindValue(":videoId", $_POST["videoId"]);
+
+    $query->execute();
+    echo $query->fetchColumn();
+
+
+}
+else {
+    echo "No videoId or username passed into file";
+}
+?>
